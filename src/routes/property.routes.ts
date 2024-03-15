ae1c09c30ab54de1adc070d1504df5154e2655c6
alanloffler <alanmatiasloffler@gmail.com>
2024-03-15 13:45:19 -0300

    Added routes

--- src/routes/property.routes.ts
+++ src/routes/property.routes.ts
@@ -4,9 +4,10 @@ import {PropertyController } from '../controllers/property.controller';
 const router: Router = Router();
 
 router.get('/properties', PropertyController.getAll);
-router.get('/properties/:id', PropertyController.getById);
-
+router.post('/properties', PropertyController.create);
+router.get('/properties/:id', PropertyController.getOne);
 router.put('/properties/:id', PropertyController.update);
 router.patch('/properties/:id', PropertyController.setActive);
+router.delete('/properties/:id', PropertyController.delete);
 
 export default router;
