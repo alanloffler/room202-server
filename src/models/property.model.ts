e47fe317df4a8689fc3d10d8cb081d9f46d5c766
alanloffler <alanmatiasloffler@gmail.com>
2024-03-15 13:45:47 -0300

    Added crud functions

--- src/models/property.model.ts
+++ src/models/property.model.ts
@@ -3,43 +3,51 @@ import pool from '../db';
 import { IProperty } from '../interfaces/property.interface';
 
 export class PropertyModel {
-    static async getAll(): Promise<IProperty[]> {
-        const connection = await pool.getConnection();
-        const sql: string = 'SELECT * FROM properties ORDER BY id DESC';
-        const [rows] = await connection.query<IProperty[]>(sql);
-        connection.release();
-        return rows;
-    }
+  static async getAll(): Promise<IProperty[]> {
+    const connection = await pool.getConnection();
+    const sql: string = 'SELECT * FROM properties ORDER BY id DESC';
+    const [rows] = await connection.query<IProperty[]>(sql);
+    connection.release();
+    return rows;
+  }
 
-    static async getOne(id: number): Promise<IProperty> {
-        const connection = await pool.getConnection();
-        const sql: string = 'SELECT * FROM properties WHERE id=?';
-        const [rows] = await connection.query<IProperty[]>(sql, [id]);
-        connection.release();
-        return rows[0];
-    }
-    // TODO ADD AND UPDATE
-    static async deleteProperty(id: number): Promise<ResultSetHeader> {
-        const connection = await pool.getConnection();
-        const sql: string = 'DELETE FROM properties WHERE id=';
-        const [rows] = await connection.query<ResultSetHeader>(sql, [id]);
-        connection.release();
-        return rows;
-    }
+  static async getOne(id: number): Promise<IProperty> {
+    const connection = await pool.getConnection();
+    const sql: string = 'SELECT * FROM properties WHERE id=?';
+    const [rows] = await connection.query<IProperty[]>(sql, [id]);
+    connection.release();
+    return rows[0];
+  }
 
-    static async setActive(id: number, active: boolean): Promise<ResultSetHeader> {
-        const connection = await pool.getConnection();
-        const sql: string = 'UPDATE properties SET is_active=? WHERE id=?';
-        const [rows] = await connection.query<ResultSetHeader>(sql, [active, id]);
-        connection.release();
-        return rows;
-    }
+  static async create(data: object): Promise<ResultSetHeader> {
+    const connection = await pool.getConnection();
+    const sql: string = 'INSERT INTO properties SET ?';
+    const [rows] = await connection.query<ResultSetHeader>(sql, [data]);
+    connection.release();
+    return rows;
+  }
 
-    static async update(id: number, data: IProperty): Promise<ResultSetHeader> {
-        const connection = await pool.getConnection();
-        const sql: string = 'UPDATE properties SET ? WHERE id=?';
-        const [rows] = await connection.query<ResultSetHeader>(sql, [data, id]);
-        connection.release();
-        return rows;
-    }
+  static async update(id: number, data: IProperty): Promise<ResultSetHeader> {
+    const connection = await pool.getConnection();
+    const sql: string = 'UPDATE properties SET ? WHERE id=?';
+    const [rows] = await connection.query<ResultSetHeader>(sql, [data, id]);
+    connection.release();
+    return rows;
+  }
+
+  static async delete(id: number): Promise<ResultSetHeader> {
+    const connection = await pool.getConnection();
+    const sql: string = 'DELETE FROM properties WHERE id=?';
+    const [rows] = await connection.query<ResultSetHeader>(sql, [id]);
+    connection.release();
+    return rows;
+  }
+  
+  static async setActive(id: number, active: boolean): Promise<ResultSetHeader> {
+    const connection = await pool.getConnection();
+    const sql: string = 'UPDATE properties SET is_active=? WHERE id=?';
+    const [rows] = await connection.query<ResultSetHeader>(sql, [active, id]);
+    connection.release();
+    return rows;
+  }
 }
